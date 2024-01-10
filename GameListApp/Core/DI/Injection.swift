344e821c2e66c32ebbe2356ba4c4814e0dfe824e
Nadia Ramadhani <103446529+nadiaramadhani@users.noreply.github.com>
2024-01-10 13:55:17 +0700

    Add files via upload

---
+++ GameListApp/Core/DI/Injection.swift
@@ -0,0 +1,8 @@
+//
+//  Injection.swift
+//  GameListApp
+//
+//  Created by Local Administrator on 10/01/24.
+//
+
+import Foundation
---
+++ GameListApp/GameListApp/GameListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameListAppApp.swift
+//  GameListApp
+//
+//  Created by Local Administrator on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GameListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
